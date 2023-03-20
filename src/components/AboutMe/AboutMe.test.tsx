16d23c925ad905d85186a479c28980b60c822248
Eva Kerrigan <evakerrigan@gmail.com>
2023-03-20 04:38:09 +0300

    refactor: change title test

--- src/components/AboutMe/AboutMe.test.tsx
+++ src/components/AboutMe/AboutMe.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { AboutMe } from './AboutMe';
 
-test('CardItem renders correctly', () => {
+test('AboutMe renders correctly', () => {
   render(<AboutMe />);
   const textElement = screen.getByText('AboutMe');
   expect(textElement).toBeInTheDocument();
