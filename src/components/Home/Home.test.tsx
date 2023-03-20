1dc56acaaf3d80af7cb346f23d03c63ef81f270f
Eva Kerrigan <evakerrigan@gmail.com>
2023-03-20 04:37:48 +0300

    feat: add else one test for card list

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { Home } from './Home';
 
 test('CardItem renders correctly', () => {
   render(<Home />);
   const textElement = screen.getByText('Home');
   expect(textElement).toBeInTheDocument();
+});
+
+describe('CardList renders correctly', () => {
+  it('renders Card', () => {
+    render(<Home />);
+    const images = screen.getAllByRole('img');
+    expect(images.length).toBe(9);
+  });
 });
