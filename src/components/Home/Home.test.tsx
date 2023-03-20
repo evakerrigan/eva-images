09eb040733a1c53bcf799d09a97457f31bc2e72b
Eva Kerrigan <evakerrigan@gmail.com>
2023-03-20 04:58:32 +0300

    refactor: small edits

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import { Home } from './Home';
 
 test('CardItem renders correctly', () => {
