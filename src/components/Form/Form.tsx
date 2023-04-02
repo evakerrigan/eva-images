2b6623449ad2cbfb5320ed0cc1b4f0647f02aaf8
Eva Kerrigan <evakerrigan@gmail.com>
2023-04-02 22:26:12 +0300

    feat: add form - react hook form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,6 @@
-import React, { Component, createRef, FormEvent, RefObject } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import './Form.scss';
-import { InputDate } from './InputDate/InputDate';
-import { InputFile } from './InputFile/InputFile';
-import { InputSelect } from './InputSelect/InputSelect';
-import { InputTitle } from './InputTitle/InputTitle';
-import { InputStyle } from './InputStyle/InputStyle';
-import { InputAgree } from './InputAgree/InputAgree';
 import { SalePhotoCard } from '../SalePhotoCard/SalePhotoCard';
 import { Modal } from '../Modal/Modal';
 
@@ -16,14 +11,6 @@ export type typePropsElseBimbo = {
 interface myState {
   saleList: myCard[];
   modal: boolean;
-  errors: {
-    refFile: boolean;
-    refTitle: boolean;
-    refSelect: boolean;
-    refDate: boolean;
-    refStyle: boolean;
-    refAgree: boolean;
-  };
 }
 
 interface myCard {
@@ -34,119 +21,169 @@ interface myCard {
   style: string;
 }
 
-export class Form extends Component<typePropsElseBimbo, myState> {
-  private refFile: RefObject<HTMLInputElement>;
-  private refTitle: RefObject<HTMLInputElement>;
-  private refSelect: RefObject<HTMLSelectElement>;
-  private refDate: RefObject<HTMLInputElement>;
-  private refStyleLifestyle: RefObject<HTMLInputElement>;
-  private refStyleStudio: RefObject<HTMLInputElement>;
-  private refAgree: RefObject<HTMLInputElement>;
-  private refForm: RefObject<HTMLFormElement>;
+export function Form(
+  props: { [key: string]: typePropsElseBimbo },
+  state: { saleList: myState; modal: myState }
+) {
+  state = {
+    saleList: [],
+    modal: false,
+  };
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
 
-  constructor(props: typePropsElseBimbo) {
-    super(props);
-    this.refFile = createRef();
-    this.refTitle = createRef();
-    this.refSelect = createRef();
-    this.refDate = createRef();
-    this.refStyleLifestyle = createRef();
-    this.refStyleStudio = createRef();
-    this.refAgree = createRef();
-    this.refForm = createRef();
-    this.state = {
-      saleList: [],
-      modal: false,
-      errors: {
-        refFile: false,
-        refTitle: false,
-        refSelect: false,
-        refDate: false,
-        refStyle: false,
-        refAgree: false,
-      },
-    };
-  }
-  handleButton(e: FormEvent) {
-    e.preventDefault();
-    const photo = this.refFile.current as HTMLInputElement;
-    const title = (this.refTitle.current as HTMLInputElement).value;
-    const select = (this.refSelect.current as HTMLSelectElement).value;
-    const date = (this.refDate.current as HTMLInputElement).value;
-    const styleLifestyle = this.refStyleLifestyle?.current?.checked;
-    const styleStudio = this.refStyleStudio?.current?.checked;
-    let style: string | undefined;
-    if (styleLifestyle) {
-      style = this.refStyleLifestyle?.current?.value;
-    } else if (styleStudio) {
-      style = this.refStyleStudio?.current?.value;
-    }
-    const agree = this.refAgree?.current?.checked;
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    console.log(data);
+    console.log(errors);
+    reset();
+  };
 
-    const er = {
-      refFile: false,
-      refTitle: false,
-      refSelect: false,
-      refDate: false,
-      refStyle: false,
-      refAgree: false,
-    };
-
-    photo.value.length > 0 ? null : (er.refFile = true);
-    title.length > 0 && title[0] == title[0].toUpperCase() ? null : (er.refTitle = true);
-    select !== 'select' ? null : (er.refSelect = true);
-    date.length > 0 ? null : (er.refDate = true);
-    style ? null : (er.refStyle = true);
-    agree ? null : (er.refAgree = true);
-
-    this.setState({ errors: er });
-    if (Object.values(er).every((item) => !item)) {
-      const card = {
-        photo: URL.createObjectURL(((photo as HTMLInputElement).files as FileList)[0]),
-        title: title,
-        select: select,
-        date: date,
-        style: style ? style : '',
-      };
-      this.setState({ saleList: [...this.state.saleList, card] });
-      this.setState({ modal: true });
-      setTimeout(() => {
-        this.setState({ modal: false });
-      }, 5000);
-      this.refForm?.current ? this.refForm?.current.reset() : '';
-    }
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <form ref={this.refForm} className="input-form">
-          <fieldset className="input-fieldset">
-            <legend className="input-legend-title">Form for your:</legend>
-            <InputFile refFile={this.refFile} er={this.state.errors.refFile} />
-            <InputTitle refTitle={this.refTitle} er={this.state.errors.refTitle} />
-            <InputSelect refSelect={this.refSelect} er={this.state.errors.refSelect} />
-            <InputDate refDate={this.refDate} er={this.state.errors.refDate} />
-            <InputStyle
-              refStyleLifestyle={this.refStyleLifestyle}
-              refStyleStudio={this.refStyleStudio}
-              er={this.state.errors.refStyle}
-            />
-            <InputAgree refAgree={this.refAgree} er={this.state.errors.refAgree} />
-            <div className="input-button-wrapper">
-              <button onClick={(e) => this.handleButton(e)} className="form-button">
-                Отправить
-              </button>
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleSubmit(onSubmit)} className="input-form">
+        <fieldset className="input-fieldset">
+          <legend className="input-legend-title">Form for your:</legend>
+          <input
+            type="file"
+            {...register('inputFile', { required: true })}
+            accept="image/png, image/jpeg"
+            className="input input-image file"
+          />
+          <div>{errors?.inputFile && <p className="input-error">Нет картинки</p>}</div>
+          <input
+            type="text"
+            {...register('inputTitle', { required: true })}
+            className="input input-text title"
+          />
+          <div>
+            {errors?.inputTitle && (
+              <p className="input-error">
+                Название фото должно содержать хотя бы 1 букву и должно начинаться с заглавной
+              </p>
+            )}
+          </div>
+          <select
+            {...register('inputSelect', { required: true })}
+            className="input input-select stock"
+          >
+            <option value="" selected disabled>
+              Select Stock
+            </option>
+            <option value="adobe">Adobe</option>
+            <option value="getty">Getty</option>
+            <option value="offset">Offset</option>
+          </select>
+          <div>{errors?.inputSelect && <p className="input-error">Не выбран фотосток</p>}</div>
+          <input
+            type="date"
+            {...register('inputDate', { required: true })}
+            className="input input-date date"
+          />
+          <div>{errors?.inputDate && <p className="input-error">Нет даты съемки</p>}</div>
+          <div>
+            <legend className="input-legend">Choose the style of your photo:</legend>
+            <div className="input-wrapper">
+              <input
+                type="radio"
+                {...register('inputStyle', { required: true })}
+                id="one"
+                name="style"
+                value="lifestyle"
+                className="input input-radio choice"
+              />
+              <label htmlFor="one">Lifestyle</label>
             </div>
-          </fieldset>
-        </form>
-        <div className="output">
-          {this.state.saleList.map((item, i) => (
-            <SalePhotoCard key={i} item={item} />
-          ))}
-        </div>
-        {this.state.modal === true ? <Modal /> : null}
+            <div className="input-wrapper">
+              <input
+                type="radio"
+                {...register('inputStyle', { required: true })}
+                id="two"
+                name="style"
+                value="studio"
+                className="input input-radio choice"
+              />
+              <label htmlFor="two">Studio style</label>
+            </div>
+          </div>
+          <div>
+            {errors?.inputStyle && <p className="input-error">Не выбран стиль фотографии</p>}
+          </div>
+          <div>
+            <legend className="input-legend">
+              I agree to send my beautiful authors photo for sale and give all rights to it to Eva
+              Images:
+            </legend>
+            <div className="input-wrapper">
+              <input
+                type="checkbox"
+                {...register('inputAgree', { required: true })}
+                id="2"
+                className="input input-checkbox agree"
+              />
+              <label htmlFor="agree">I agree</label>
+            </div>
+          </div>
+          <div>
+            {errors?.inputAgree && <p className="input-error">Не дано согласие на продажу фото</p>}
+          </div>
+
+          <div className="input-button-wrapper">
+            <input type="submit" className="form-button" />
+          </div>
+        </fieldset>
+      </form>
+      {/* <div className="output">
+        {state.saleList.map((item, i) => (
+          <SalePhotoCard key={i} item={item} />
+        ))}
       </div>
-    );
-  }
+      {state.modal === true ? <Modal /> : null} */}
+    </div>
+  );
 }
+
+// handleButton(e: FormEvent) {
+//   e.preventDefault();
+//   const photo = refFile.current as HTMLInputElement;
+//   const title = (trefTitle.current as HTMLInputElement).value;
+//   const select = (refSelect.current as HTMLSelectElement).value;
+//   const date = (trefDate.current as HTMLInputElement).value;
+//   const styleLifestyle = refStyleLifestyle?.current?.checked;
+//   const styleStudio = refStyleStudio?.current?.checked;
+//   let style: string | undefined;
+//   if (styleLifestyle) {
+//     style = refStyleLifestyle?.current?.value;
+//   } else if (styleStudio) {
+//     style = refStyleStudio?.current?.value;
+//   }
+//   const agree = refAgree?.current?.checked;
+
+//   photo.value.length > 0 ? null : (er.refFile = true);
+//   title.length > 0 && title[0] == title[0].toUpperCase() ? null : (er.refTitle = true);
+//   select !== 'select' ? null : (er.refSelect = true);
+//   date.length > 0 ? null : (er.refDate = true);
+//   style ? null : (er.refStyle = true);
+//   agree ? null : (er.refAgree = true);
+
+//   setState({ errors: er });
+//   if (Object.values(er).every((item) => !item)) {
+//     const card = {
+//       photo: URL.createObjectURL(((photo as HTMLInputElement).files as FileList)[0]),
+//       title: title,
+//       select: select,
+//       date: date,
+//       style: style ? style : '',
+//     };
+//     setState({ saleList: [...this.state.saleList, card] });
+//     setState({ modal: true });
+//     setTimeout(() => {
+//       setState({ modal: false });
+//     }, 5000);
+//     refForm?.current ? refForm?.current.reset() : '';
+//   }
+// }
