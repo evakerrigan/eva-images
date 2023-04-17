0d33bbb5db20ba7ab9fad0bfc9b09d3ce3badb6d
Eva Kerrigan <evakerrigan@gmail.com>
2023-04-17 08:35:19 +0300

    feat: add search save at redux stor

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,32 @@ import { Footer } from './components/Footer/Footer';
 import { PageWrapper } from './components/PageWrapper/PageWrapper';
 
 function App() {
-  const a = localStorage.getItem('inputValue');
-
-  const [query, setQuery] = useState(a ? a : '');
   return (
     <div className="App">
       <Routes>
         <Route
           index
           element={
-            <PageWrapper setQuery={setQuery} title="Home" content={<Home query={query} />} />
+            <PageWrapper title="Home" content={<Home />} />
           }
         />
         <Route
           path="/"
           element={
-            <PageWrapper setQuery={setQuery} title="Home" content={<Home query={query} />} />
+            <PageWrapper title="Home" content={<Home />} />
           }
         />
         <Route
           path="/about-me"
-          element={<PageWrapper setQuery={setQuery} title="AboutMe" content={<AboutMe />} />}
+          element={<PageWrapper title="AboutMe" content={<AboutMe />} />}
         />
         <Route
           path="/notfound"
-          element={<PageWrapper setQuery={setQuery} title="NotFound" content={<NotFound />} />}
+          element={<PageWrapper title="NotFound" content={<NotFound />} />}
         />
         <Route
           path="/form"
-          element={<PageWrapper setQuery={setQuery} title="Form" content={<Form />} />}
+          element={<PageWrapper title="Form" content={<Form />} />}
         />
         <Route path="*" element={<Navigate to="/notfound" replace />} />
       </Routes>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-
-import { inputReducer } from './slices/inputSlice';
+import { StoreState } from './types';
+import { searchSlice } from './search';
+import { cardsSlice } from './cards';
 // import searchReducer from './slices/STATE_SEARCH_REDUCER';
 // import formReducer from './slices/FORM_REDUCER';
 
-export const store = configureStore({
+export const store = configureStore<StoreState>({
   reducer: {
     // form: formReducer,
-    // search: searchReducer,
-    // input – это свойство будет внутри объекта общего состояния: state.input
-    input: inputReducer,
+    search: searchSlice.reducer,
+    cardsState: cardsSlice.reducer,
   },
 });
